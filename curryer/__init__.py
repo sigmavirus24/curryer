3cb5c20efff302a55ac97045a5f4a3d973aee915
Ian Cordasco <graffatcolmingov@gmail.com>
2013-11-09 20:21:57 -0600

    More setup for packaging

--- curryer/__init__.py
+++ curryer/__init__.py
@@ -1,3 +1,19 @@
+"""
+curryer
+=======
+
+See http://curryer.rtfd.org/ for documentation.
+
+:copyright: (c) 2013 by Ian Cordasco
+:license: Modified BSD, see LICENSE for more details
+
+"""
+
 from .api import curry
 
 __all__ = [curry]
+__author__ = 'Ian Cordasco'
+__copyright__ = 'Copyright 2013 Ian Cordasco'
+__license__ = 'Modified BSD'
+__version__ = '0.1.0'
+__version_info__ = tuple(int(i) for i in __version__.split('.'))
---
+++ setup.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+import curryer
+import os
+import sys
+
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+kwargs = {}
+requires = []
+packages = ['curryer']
+
+kwargs['tests_require'] = ['pytest==2.3.5']
+packages.append('tests')
+
+if sys.argv[-1] in ('submit', 'publish'):
+    os.system('python setup.py bdist_wheel sdist upload')
+    sys.exit()
+
+if not (hasattr(curryer, '__version__') and curryer.__version__):
+    raise RuntimeError('Cannot find version information')
+
+__version__ = curryer.__version__
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = ['-q', 'tests/']
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
+setup(
+    name='curryer',
+    version=__version__,
+    description='Haskell style currying for Python3.3+ callables',
+    long_description="\n\n".join([open("README.rst").read(),
+                                  open("HISTORY.rst").read()]),
+    license=open('LICENSE').read(),
+    author="Ian Cordasco",
+    author_email="graffatcolmingov@gmail.com",
+    url="https://curryer.readthedocs.org",
+    packages=packages,
+    package_data={'': ['LICENSE', 'AUTHORS.rst']},
+    include_package_data=True,
+    install_requires=requires,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+    extras_require={'test': kwargs['tests_require']},
+    cmdclass={'test': PyTest},
+    **kwargs
+)
