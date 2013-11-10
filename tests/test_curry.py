f35a015657bdcf3a149b31026eebe454004fd7ef
Ian Cordasco <graffatcolmingov@gmail.com>
2013-11-09 20:05:19 -0600

    Add more tests

--- tests/test_curry.py
+++ tests/test_curry.py
@@ -13,5 +13,21 @@ class TestCurry:
         assert func.curried is False
 
     def test_curry_refuses_None(self):
-        with pytest.raises(ValueError):
+        """Ensure that currypy.curry refuses None"""
+        with pytest.raises(TypeError):
             curry(None)
+
+    def test_curries_when_given_parameters(self):
+        @curry
+        def add(a, b):
+            return a + b
+
+        assert add(1).curried is True
+
+    def test_evaluates_when_given_enough_parameters(self):
+        @curry
+        def add(a, b):
+            return a + b
+
+        assert add(1)(2) == 3
+        assert add(1, 2) == 3
