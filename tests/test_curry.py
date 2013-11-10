be7749ef71ef2b5817200273e68652c6203c8c0b
Ian Cordasco <graffatcolmingov@gmail.com>
2013-11-09 21:37:36 -0600

    More tests

--- tests/test_curry.py
+++ tests/test_curry.py
@@ -44,3 +44,22 @@ class TestCurry:
         add_more = add_multiple(2, 3, 4, 5)
         assert add_more.curried is True
         assert add_more() == 15
+
+    def test_variadic_keyword_arguments_behave_differently(self):
+        @curry
+        def join(**kwargs):
+            return kwargs
+
+        join_with_one = join(one=1)
+        assert join_with_one.curried is True
+        assert join_with_one() == {'one': 1}
+
+        join_others = join_with_one(two=2, three=3, four=4, five=5)
+        assert join_others.curried is True
+        assert join_others() == {
+            'one': 1,
+            'two': 2,
+            'three': 3,
+            'four': 4,
+            'five': 5
+            }
