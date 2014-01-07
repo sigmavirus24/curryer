32ae29c38f99b76ffe938eb836df64d9d84f84b3
Ian Cordasco <graffatcolmingov@gmail.com>
2014-01-06 19:47:16 -0600

    Update copyright year

--- curryer/__init__.py
+++ curryer/__init__.py
@@ -4,7 +4,7 @@ curryer
 
 See http://curryer.rtfd.org/ for documentation.
 
-:copyright: (c) 2013 by Ian Cordasco
+:copyright: (c) 2014 by Ian Cordasco
 :license: Modified BSD, see LICENSE for more details
 
 """
@@ -13,7 +13,7 @@ from .api import curry
 
 __all__ = [curry]
 __author__ = 'Ian Cordasco'
-__copyright__ = 'Copyright 2013 Ian Cordasco'
+__copyright__ = 'Copyright 2014 Ian Cordasco'
 __license__ = 'Modified BSD'
 __version__ = '0.1.0'
 __version_info__ = tuple(int(i) for i in __version__.split('.'))
