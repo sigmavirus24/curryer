6845383b85baf7e30a5ffb9b4a019f19b108bcfa
Ian Cordasco <graffatcolmingov@gmail.com>
2014-01-06 19:46:59 -0600

    Docstring for curry function

--- curryer/api.py
+++ curryer/api.py
@@ -2,4 +2,21 @@ from .curry import Curry
 
 
 def curry(wrapped_callable):
+    """
+    This is the public API to curryer.
+
+    This function is meant to be used as a decorator like so::
+
+        @curry
+        def add(a, b):
+            return a + b
+
+    One can then use ``add`` like so::
+
+        add_one = add(1)
+        print(add_one(2))  # => 3
+        add_two = add(2)
+        print(add_two(3))  # => 5
+
+    """
     return Curry(wrapped_callable)
